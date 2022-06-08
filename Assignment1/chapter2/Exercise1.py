f30d161e11a2b7a21052b560612aae27b3f284b7
maxfaber <maxfaber@quicknet.nl>
2022-06-08 09:13:23 +0200

    Added the PDF of the book and definitions.py which contains the ROOT_DIR

--- Assignment1/chapter2/Exercise1.py
+++ Assignment1/chapter2/Exercise1.py
@@ -1,10 +1,13 @@
 from Python3Code.Chapter2.CreateDataset import CreateDataset
+from definitions import ROOT_DIR
 
-def plotSensorData -> None:
+def plotSensorData() -> None:
     GRANULARITIES = [60000, 250]
 
     for milliseconds_per_instance in GRANULARITIES:
-        dataset = CreateDataset('./d', milliseconds_per_instance)
+        dataset: CreateDataset = CreateDataset(f'{ROOT_DIR}/phyphox_exports', milliseconds_per_instance)
+
+        dataset.add_numerical_dataset('Gyroscoop ruis.csv', 'Time (s)', ['Gyroscope x (rad/s)', 'Gyroscope y (rad/s)', 'Gyroscope z (rad/s)'], 'avg', 'acc_phone_noise_')
 
 if __name__ == '__main__':
-    pass
+    plotSensorData()
